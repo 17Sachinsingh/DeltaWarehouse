8d0dd9feb950ad20337ccbbf21d867d2979a918c
17SachinSingh <141268460+NitinSingh0@users.noreply.github.com>
2024-07-12 19:50:27 +0530

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using DeltaWarehouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeltaWarehouse.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public List<ProductService> prod;
+        public ProductsController()
+        {
+            prod = new List<ProductService>()
+            {
+                new ProductService() { ProductId = 1001, Name = "Tools Gears", Description = "Gear tools production specification" },
+                new ProductService() { ProductId = 2001, Name = "Panel", Description = "Switch Gear Panels " },
+                new ProductService() { ProductId = 2008, Name = "DocTonar", Description = "Document Solar Objects" },
+            };
+        }
+        public IActionResult Index()
+        {
+            return View(prod);
+        }
+    }
+}
---
+++ Models/ProductService.cs
@@ -0,0 +1,9 @@
+﻿namespace DeltaWarehouse.Models
+{
+    public class ProductService
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
